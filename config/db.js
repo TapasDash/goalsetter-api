adf700d2f4dbfe70ea7a20e1d2f739f0cfbc7747
TapasDash <tapasdash017@gmail.com>
2023-01-13 22:50:06 +0530

    added env for mysql url

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 import { Sequelize } from "sequelize";
-
+import dotenv from "dotenv";
+dotenv.config();
 // const db = new Sequelize("railway", "root", "0wxWtWnoS85kAqHj3kG8", {
 //   host: "containers-us-west-121.railway.app",
 //   dialect: "mysql",
 // });
 
-const db = new Sequelize(
-  "mysql://root:0wxWtWnoS85kAqHj3kG8@containers-us-west-121.railway.app:7628/railway"
-);
+const db = new Sequelize(process.env.MY_SQL_URL);
 
 export default db;
